eea3cdb555bd4a4ca75b045713f701db4066b8d1
nahidnub1126 <nahidnub1126@gmail.com>
2022-09-25 17:44:00 +0600

    add diffUtil and clicklistener

--- DdatabindingWithRecyclerView/app/src/main/java/com/example/ddatabindingwithrecyclerview/MainActivity.kt
+++ DdatabindingWithRecyclerView/app/src/main/java/com/example/ddatabindingwithrecyclerview/MainActivity.kt
@@ -9,16 +9,19 @@ import com.example.ddatabindingwithrecyclerview.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val userList = ArrayList<User>()
+    private lateinit var userAdapter: UserAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+       setContentView(binding.root)
 
+        userAdapter = UserAdapter()
         createUser()
-        val recyclerView = binding.recyclerView
-        val userAdapter = UserAdapter(this,userList)
-
-        recyclerView.adapter = userAdapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(applicationContext)
+            adapter = userAdapter
+        }
+        userAdapter.setUser(userList);
     }
 
     private fun createUser() {
--- DdatabindingWithRecyclerView/app/src/main/java/com/example/ddatabindingwithrecyclerview/UserAdapter.kt
+++ DdatabindingWithRecyclerView/app/src/main/java/com/example/ddatabindingwithrecyclerview/UserAdapter.kt
@@ -1,18 +1,25 @@
 package com.example.ddatabindingwithrecyclerview
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ddatabindingwithrecyclerview.databinding.RowItemBinding
 
-class UserAdapter(private val context: Context, private var userList: List<User>) :
+class UserAdapter() :
     RecyclerView.Adapter<UserAdapter.ViewHolder>() {
-
+    private val TAG = "UserAdapter"
     private lateinit var binding: RowItemBinding
+    private  var userList = ArrayList<User>()
+    private lateinit var mContext: Context
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        binding = RowItemBinding.inflate(LayoutInflater.from(context), parent, false)
+        mContext = parent.context
+        binding = RowItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
@@ -25,9 +32,20 @@ class UserAdapter(private val context: Context, private var userList: List<User>
         return userList.size
     }
 
-    class ViewHolder(private val binding: RowItemBinding) : RecyclerView.ViewHolder(binding.root) {
+   inner class ViewHolder(private val binding: RowItemBinding) : RecyclerView.ViewHolder(binding.root) {
         fun setData(user: User) {
             binding.user = user
+            binding.setSubmitClickListener {
+                Toast.makeText(mContext, "Clicked", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    //this is diffUtils for notify dataset change
+    fun setUser(newList: ArrayList<User>){
+        val diffUtils = UserDiffUtils(userList,newList);
+        val diffResult = DiffUtil.calculateDiff(diffUtils)
+        userList = newList
+        diffResult.dispatchUpdatesTo(this)
     }
 }
